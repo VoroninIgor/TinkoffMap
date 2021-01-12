b2eb2b3c60be3b2cc112d044deedf4dc9888f10b
angai <angainor17@gmail.com>
2021-01-12 22:25:04 +0300

    ViewPager

--- app/src/main/java/com/voronin/tinkoff/presentation/depositionpoints/viewer/DepositionPointsViewerFragment.kt
+++ app/src/main/java/com/voronin/tinkoff/presentation/depositionpoints/viewer/DepositionPointsViewerFragment.kt
@@ -1,22 +1,60 @@
 package com.voronin.tinkoff.presentation.depositionpoints.viewer
 
+import android.content.Context
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
 import com.voronin.tinkoff.R
 import com.voronin.tinkoff.presentation.base.BaseFragment
+import com.voronin.tinkoff.presentation.depositionpoints.list.DepositionPointsListFragment
+import com.voronin.tinkoff.presentation.depositionpoints.map.DepositionPointsMapFragment
+import kotlinx.android.synthetic.main.fragment_depositions_points_viewer.tabsDepositionsPoint
+import kotlinx.android.synthetic.main.fragment_depositions_points_viewer.viewPagerDepositionsPoint
 
 class DepositionPointsViewerFragment : BaseFragment(R.layout.fragment_depositions_points_viewer) {
 
     private val viewModel: DepositionPointsViewerViewModel by appViewModels()
+
+    private val adapter by lazy {
+        DepositionPointsPagerAdapter(
+            requireContext(),
+            childFragmentManager
+        )
+    }
 
     override fun callOperations() {
 
     }
 
     override fun onSetupLayout(savedInstanceState: Bundle?) {
-
+        viewPagerDepositionsPoint.adapter = adapter
+        tabsDepositionsPoint.setupWithViewPager(viewPagerDepositionsPoint)
     }
 
     override fun onBindViewModel() = with(viewModel) {
 
+    }
+
+    class DepositionPointsPagerAdapter(
+        private val context: Context,
+        fragmentManager: FragmentManager,
+    ) : FragmentStatePagerAdapter(
+        fragmentManager,
+        BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
+    ) {
+
+        private val list = arrayOf(
+            DepositionPointsMapFragment() to R.string.app_name,
+            DepositionPointsListFragment() to R.string.error_refresh
+        )
+
+        override fun getCount(): Int = list.size
+
+        override fun getItem(position: Int): Fragment = list[position].first
+
+        override fun getPageTitle(position: Int): CharSequence {
+            return context.getString(list[position].second)
+        }
     }
 }
