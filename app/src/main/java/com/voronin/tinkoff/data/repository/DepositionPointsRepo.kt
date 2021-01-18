4a2f30f4b2a4dfad411e61ab20d738c5b0ab01eb
angai <angainor17@gmail.com>
2021-01-18 11:04:59 +0300

    refactoring

--- app/src/main/java/com/voronin/tinkoff/data/repository/DepositionPointsRepo.kt
+++ app/src/main/java/com/voronin/tinkoff/data/repository/DepositionPointsRepo.kt
@@ -46,7 +46,8 @@ class DepositionPointsRepo @Inject constructor(
                     database.depositionPointReqDao().delete(req) // удаление запроса по истечению времени
                     database.requestWithDepositionPointEntityDao().delete(req.id)
                 } else {
-                    if (req.latitude == latitude && req.longitude == longitude && req.radius == radius && it.points.isNotEmpty()) {
+                    val isReqHasSameLocation = req.latitude == latitude && req.longitude == longitude && req.radius == radius
+                    if (isReqHasSameLocation && it.points.isNotEmpty()) {
                         return@flatMap Single.just(
                             it.points.map { item -> depositionMapper.fromEntityToModel(item) } // запрос найден в кеше
                         )
